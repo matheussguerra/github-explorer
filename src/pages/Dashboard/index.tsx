e82e44768cb58833d81efa8f2c9ceaa74d8478d7
GuerraUTFPR <guerramatheus2@gmail.com>
2020-09-18 15:38:03 -0300

    preparação para conectar api

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { FiChevronRight } from 'react-icons/fi'
 
-import { Title } from './styles'
+import api from '../../services/api'
+import logoImg from '../../assets/logo.svg';
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-    return <Title>Explore repositórios no GitHub</Title>
-}
+    const [newRepo, setNewRepo] = useState('')
+    const [repositories, setRepositories] = useState([]);
 
-export default Dashboard
+    function handleAddReposotory() {
+        //add repo
+
+    }
+
+    return (
+        <>
+            <img src={logoImg} alt="Github Explorer" />
+            <Title>Explore repositórios no GitHub</Title>
+
+            <Form action="">
+                <input
+                    placeholder="Digite o nome do repositório" />
+                <button type="submit">Pesquisar</button>
+            </Form>
+
+            <Repositories>
+                <a href="teste">
+                    <img src="https://avatars3.githubusercontent.com/u/22222919?s=460&u=3d27e0999cd4ec472aa121b78d79145f3e711cef&v=4"
+                        alt="Matheus Guerra">
+                    </img>
+                    <div>
+                        <strong>matheussguerra/node</strong>
+                        <p>Aprendendo a usar node</p>
+                    </div>
+                    <FiChevronRight size={20} />
+
+
+                </a>
+            </Repositories>
+        </>
+    );
+
+}
+export default Dashboard;
