4833335b3cfc7abb2ccf8a721372f17b8c0bfc92
GuerraUTFPR <guerramatheus2@gmail.com>
2020-09-21 14:01:48 -0300

    estilização de repos

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,14 +1,63 @@
 import React from 'react'
-import {useRouteMatch} from 'react-router-dom'
+import { useRouteMatch, Link } from 'react-router-dom'
+import { FiChevronLeft, FiChevronRight } from 'react-icons/fi'
 
+import logoImg from '../../assets/logo.svg';
+import { Header, RepositoryInfo, Issues } from './styles'
 
-interface RepositoryParams{
+interface RepositoryParams {
     repository: string;
 }
 const Repository: React.FC = () => {
     const { params } = useRouteMatch<RepositoryParams>();
 
-    return <h1>Repository:{params.repository}</h1>
+    return (
+        <>
+            <Header>
+                <img src={logoImg} alt="Github Explorer" />
+                <Link to="/">
+                    <FiChevronLeft size={16} />
+                Voltar
+            </Link>
+            </Header>
+            <RepositoryInfo>
+                <header>
+                    <img src="https://avatars0.githubusercontent.com/u/28929274?v=4" alt="Rocketseat" />
+                    <div>
+                        <strong>Rocketseat/unform</strong>
+                        <p>descrição</p>
+                    </div>
+                </header>
+                <ul>
+                    <li>
+                        <strong>1080</strong>
+                        <span>Starts</span>
+                    </li>
+                    <li>
+                        <strong>48</strong>
+                        <span>Forks</span>
+                    </li>
+                    <li>
+                        <strong>67</strong>
+                        <span>issues</span>
+                    </li>
+                </ul>
+            </RepositoryInfo>
+
+            <Issues>
+                <Link
+                    to='kkk'
+                >
+                    <div>
+                        <strong>kkk</strong>
+                        <p>kkk</p>
+                    </div>
+                    <FiChevronRight size={20} />
+                </Link>
+            </Issues>
+
+        </>
+    );
 }
 
 export default Repository
